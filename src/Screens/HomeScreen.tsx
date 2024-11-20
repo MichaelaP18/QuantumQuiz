ce59dc39960eae9ea6633a9bffa33c530894687d
Michaela Paige <114429586+MichaelaP18@users.noreply.github.com>
2024-11-20 08:41:31 -0700

    Functionality Update
    Added buttons, JSON file, and text to the app home screen and win screen

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,19 +1,81 @@
-import React from "react";
-import { View, Text, StyleSheet, } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity, } from "react-native";
 import { navigate } from './../../node_modules/@react-navigation/routers/src/CommonActions';
+import { TextInput } from "react-native-paper";
 
 
 function HomeScreen({ navigation, route } : {navigation : any, route : any}) {
+    const [name, setName] = useState(route.params?.name || "");
+    const [number, setNumber] = useState(route.params?.number || "") 
+
     return (
     <View>
-        <Text>Welcome to Quantium Quiz!</Text>
+        <View>
+            <Text>Welcome to Quantium Quiz!</Text>
+        </View>
+
+        <View>
+            <Text>Enter Your QuarkQuizQuester Name:</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="QuarkQuestor Captain John"
+                value={name}
+                onChangeText={setName}
+            />
+            
+            <Text>Enter the Number of Questions</Text>
+            <TextInput 
+            style={styles.input}
+            placeholder="12"
+            value={number}
+            onChangeText={setNumber}
+            />
+
+        </View>
+
+        
+            
+        
+
+        <TouchableOpacity 
+            style={styles.reset}
+            onPress={() => navigation.navigate("Home Screen")}
+            >
+                Reset
+            </TouchableOpacity>
+
+        <TouchableOpacity
+            style={styles.start}
+            onPress={() => navigation.navigate("Question Screen")}
+            >
+                Start Quantium Quiz
+            </TouchableOpacity>
+
     </View>
     );
 }
 
 const styles = StyleSheet.create({
     txt: {
-        fontSize: 35,
+        borderRadius: 10,
+    },
+
+    reset: {
+        borderRadius: 10,
+        backgroundColor: 'red',
+    },
+
+    start: {
+        borderRadius: 10,
+        backgroundColor: 'teal',
+    },
+
+    input: {
+        borderRadius: 10,
+    },
+
+    container: {
+        backgroundColor: 'purple',
     },
 });
 
--- src/Screens/WinScreen.tsx
+++ src/Screens/WinScreen.tsx
@@ -1,11 +1,47 @@
 import React from "react";
-import {View, Text, StyleSheet, } from "react-native";
+import {View, Text, StyleSheet, TouchableOpacity, } from "react-native";
+import HomeScreen from "./HomeScreen";
 
 
 function WinScreen({ navigation, route } : {navigation : any, route : any}) {
+
     return (
     <View>
-        <Text>Welcome to the Win screen!</Text>
+        <View>
+            <Text>QuarkQuestor Winner!</Text>
+            <Text>{name}</Text>
+        </View>
+
+        <View style={styles.container}>
+            <Text>Correct:</Text>
+            <Text>Incorrect:</Text>
+
+            <View>
+                //Displays the number of answers that are correct and Incorrect.
+
+            </View>
+        </View>
+
+        <TouchableOpacity 
+            style={styles.againBtn}
+            onPress={() => navigation.navigate("Question Screen")}
+            >
+                Restart Quiz
+            </TouchableOpacity>
+
+        <TouchableOpacity 
+            style={styles.start}
+            onPress={() => navigation.navigate("Home Screen")}
+        >
+            Start new Quiz
+        </TouchableOpacity>
+
+        <TouchableOpacity 
+            style={styles.reset}
+            onPress={() => navigation.navigate("Home Screen")}
+        >
+            Back Home
+        </TouchableOpacity>
     </View>
     );
 }
@@ -13,6 +49,25 @@ function WinScreen({ navigation, route } : {navigation : any, route : any}) {
 const styles = StyleSheet.create({
     txt: {
         fontSize: 35,
+    },
+    
+    container: {
+        borderRadius: 15,
+    },
+
+    reset: {
+        borderRadius: 10,
+        backgroundColor: 'red',
+    },
+
+    start: {
+        borderRadius: 10,
+        backgroundColor: 'teal',
+    },
+
+    againBtn: {
+        borderRadius: 10,
+        backgroundColor: 'red',
     },
 });
 
